bec2179efc3f9f824047eeb148e33bd53f3d0fd6
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-07 07:43:27 -0300

    security: remove obsolete image origin

--- next.config.ts
+++ next.config.ts
@@ -7,10 +7,6 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
         hostname: 'hnuhabsuzaagsjrtyzdo.supabase.co',
       },
     ],
