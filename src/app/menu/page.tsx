ed5e313f886fc5c73f66bcbe43a62aeffca49864
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-08 08:04:33 -0300

    fix: import territory release scope in menu

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { territoryReleaseScope } from '@/config/territory-release-scope';
 import {
   activeTerritoryNavigationBySection,
   type TerritoryNavigationItem,
